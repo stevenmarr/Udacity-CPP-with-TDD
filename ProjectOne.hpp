6d89aee33f4507a2a3e709bbcd7496da68dda469
Steven Marr <stevenmarr@me.com>
2020-03-29 16:12:25 -0600

    compiled header file

--- ProjectOne.hpp
+++ ProjectOne.hpp
@@ -1,4 +1,20 @@
 #ifndef PROJECTONE_HPP
 #define PROJECTONE_HPP
 
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using std::cout;
+using std::ifstream;
+using std::istringstream;
+using std::string;
+using std::vector;
+
+vector<int> ParseLine(string line);
+void ReadBoardFile(string path);
+void PrintBoard(const vector<vector<int>> board);
+
 #endif /* PROJECTONE_HPP */
